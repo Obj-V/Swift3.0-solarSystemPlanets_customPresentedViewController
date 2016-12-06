c8cf00ca14ad8b0560eed35eae7b8a93a49464a8
Virata Yindeeyoungyeon <virata.ble@gmail.com>
2016-12-06 01:02:58 -0800

    Add planetView template

--- solarSystemPlanets_customPresentedViewController/PlanetDetailViewController.swift
+++ solarSystemPlanets_customPresentedViewController/PlanetDetailViewController.swift
@@ -9,27 +9,24 @@
 import UIKit
 
 class PlanetDetailViewController: UIViewController {
+    @IBOutlet weak var planetViewContainer: UIView!
+    @IBOutlet weak var planetDetailTextView: UITextView!
+    
+    var planetView : PlanetView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.loadPlanetView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func loadPlanetView() {
+        let bundle = Bundle(for: type(of: PlanetView()))
+        let nib = UINib(nibName: String(describing: type(of: PlanetView())), bundle: bundle)
+        if let planetXibView = nib.instantiate(withOwner: self, options: nil)[0] as? PlanetView {
+            self.planetView = planetXibView
+            self.planetView?.frame = self.planetViewContainer.bounds
+            self.planetView?.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+            self.planetViewContainer.addSubview(self.planetView!)
+        }
     }
-    */
-
 }
---
+++ solarSystemPlanets_customPresentedViewController/PlanetView.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetView.swift
+//  solarSystemPlanets_customPresentedViewController
+//
+//  Created by Virata Yindeeyoungyeon on 12/5/16.
+//  Copyright © 2016 ObjV. All rights reserved.
+//
+
+import UIKit
+
+
+class PlanetView: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+
+}
