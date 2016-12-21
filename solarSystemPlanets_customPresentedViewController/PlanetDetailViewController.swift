4b26b766e9fa8e78f8d5e6c424931e26b1008b7f
Virata Yindeeyoungyeon <virata.ble@gmail.com>
2016-12-20 23:53:14 -0800

    Modify PlanetView

--- solarSystemPlanets_customPresentedViewController/PlanetDetailViewController.swift
+++ solarSystemPlanets_customPresentedViewController/PlanetDetailViewController.swift
@@ -9,24 +9,12 @@
 import UIKit
 
 class PlanetDetailViewController: UIViewController {
-    @IBOutlet weak var planetViewContainer: UIView!
+
+    @IBOutlet weak var planetView: PlanetView!
     @IBOutlet weak var planetDetailTextView: UITextView!
-    
-    var planetView : PlanetView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.loadPlanetView()
     }
     
-    private func loadPlanetView() {
-        let bundle = Bundle(for: type(of: PlanetView()))
-        let nib = UINib(nibName: String(describing: type(of: PlanetView())), bundle: bundle)
-        if let planetXibView = nib.instantiate(withOwner: self, options: nil)[0] as? PlanetView {
-            self.planetView = planetXibView
-            self.planetView?.frame = self.planetViewContainer.bounds
-            self.planetView?.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
-            self.planetViewContainer.addSubview(self.planetView!)
-        }
-    }
 }
--- solarSystemPlanets_customPresentedViewController/PlanetView.swift
+++ solarSystemPlanets_customPresentedViewController/PlanetView.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
-
-
+@IBDesignable
 class PlanetView: UIView {
+
+    @IBOutlet weak var planetImageView: UIImageView!
+    @IBOutlet weak var planetNameLabel: UILabel!
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+    
+    func xibSetup() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let myview = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        myview.frame = bounds
+        addSubview(myview)
+    }
 
 }
